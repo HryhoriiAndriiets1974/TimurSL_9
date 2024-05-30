8d4420cb4584f2f6f9b0bbbb7e7f5d428a8c4639
Tymur Slynko <timurslinko1@gmail.com>
2024-05-30 16:48:47 +0300

    Optimized code and added more languages

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ from objects import Question, Answer, TestEndData
 def getResults():
     # get most scored programming language
     max_score = 0
-    max_scored_language = ""
-    for language, score in scores.items():
-        if score > max_score:
-            max_score = score
-            max_scored_language = language
+    scored_language = ""
+    for language, lang_score in scores.items():
+        if lang_score > max_score:
+            max_score = lang_score
+            scored_language = language
 
-    return programming_languages[max_scored_language if max_scored_language else "Error"]
+    return programming_languages[scored_language if scored_language else "Error"]
 
 
 programming_languages = {
@@ -75,6 +75,36 @@ programming_languages = {
         image="path/to/swift_developer_image.png"
     ),
 
+    "Unity": TestEndData(
+        title="Unity Developer",
+        desc="You are a Unity Developer!",
+        image="path/to/unity_developer_image.png"
+    ),
+
+    "SQL": TestEndData(
+        title="SQL Developer",
+        desc="You are a SQL Developer!",
+        image="path/to/sql_developer_image.png"
+    ),
+
+    "Kotlin": TestEndData(
+        title="Kotlin Developer",
+        desc="You are a Kotlin Developer!",
+        image="path/to/kotlin_developer_image.png"
+    ),
+
+    "Unreal": TestEndData(
+        title="Unreal Developer",
+        desc="You are a Unreal Developer!",
+        image="path/to/unreal_developer_image.png"
+    ),
+
+    "Rust": TestEndData(
+        title="Rust Developer",
+        desc="You are a Rust Developer!",
+        image="path/to/rust_developer_image.png"
+    ),
+
     "Error": TestEndData(
         title="Error",
         desc="An error occurred while processing the test results.",
@@ -94,42 +124,22 @@ scores = {
     "HTML": 0,
     "CSS": 0,
     "C#": 0,
-    "Swift": 0
+    "Swift": 0,
+    "Unity": 0,
+    "SQL": 0,
+    "Kotlin": 0,
+    "Unreal": 0,
+    "Rust": 0,
 }
 
 def initialize_scores():
     for language in scores:
         scores[language] = 0
 
-def on_python_answer():
-    scores["Python"] += 1
 
-def on_java_answer():
-    scores["Java"] += 1
-
-def on_cpp_answer():
-    scores["C++"] += 1
-
-def on_js_answer():
-    scores["JavaScript"] += 1
-
-def on_ruby_answer():
-    scores["Ruby"] += 1
-
-def on_php_answer():
-    scores["PHP"] += 1
-
-def on_html_answer():
-    scores["HTML"] += 1
-
-def on_css_answer():
-    scores["CSS"] += 1
-
-def on_cs_answer():
-    scores["C#"] += 1
-
-def on_swift_answer():
-    scores["Swift"] += 1
+def score(lang):
+    print(f"Scored {lang}")
+    scores[lang] += 1
 
 def create_test():
     q1 = Question(
@@ -146,10 +156,10 @@ def create_test():
                                 title="Що тобі подобається робити на веб-сайтах?",
                                 image="path/to/image3.png",
                                 answers=[
-                                    Answer("Створювати анімацію", on_css_answer),
-                                    Answer("Розробляти інтерактивні елементи", on_html_answer),
-                                    Answer("Оптимізувати сайт", on_js_answer),
-                                    Answer("Розробляти бекенд", on_php_answer)
+                                    Answer("Створювати анімацію", lambda: score("CSS")),
+                                    Answer("Розробляти інтерактивні елементи", lambda: score("HTML")),
+                                    Answer("Оптимізувати сайт", lambda: score("JavaScript")),
+                                    Answer("Розробляти бекенд", lambda: score("PHP")),
                                 ]
                             )
                         ),
@@ -158,8 +168,8 @@ def create_test():
                                 title="Якій платформі ти віддаєш перевагу?",
                                 image="path/to/image4.png",
                                 answers=[
-                                    Answer("Android", on_java_answer),
-                                    Answer("iOS", on_swift_answer)
+                                    Answer("Android", lambda: score("Java")),
+                                    Answer("iOS", lambda: score("Swift"))
                                 ]
                             )
                         ),
@@ -171,29 +181,30 @@ def create_test():
                             title="Які ігри ти найчастіше граєш?",
                             image="path/to/image5.png",
                             answers=[
-                                Answer("Ігри на ПК", on_cpp_answer, next_question=
+                                Answer("Ігри на ПК", None, next_question=
                                     Question(
                                         title="Які ігри ти віддаєш перевагу?",
                                         image="path/to/image5.png",
                                         answers=[
-                                            Answer("Ігри з відкритим світом", on_cpp_answer),
-                                            Answer("Ігри з глибоким сюжетом", on_java_answer),
-                                            Answer("Інді-ігри", on_python_answer),
-                                            Answer("Ігри на вдачу", on_ruby_answer)
+                                            Answer("Ігри з відкритим світом", lambda: score("Unreal")),
+                                            Answer("Ігри з глибоким сюжетом", lambda: score("Unreal")),
+                                            Answer("Інді-ігри", lambda: score("Unity")),
+                                            Answer("Ігри на вдачу", lambda: score("Python"))
                                         ]
                                     )
                                 ),
-                                Answer("Ігри на смартфоні", on_java_answer, next_question=
+                                Answer("Ігри на смартфоні", None, next_question=
                                        Question(
                                             title="Які ігри ти віддаєш перевагу?",
                                             image="path/to/image5.png",
                                             answers=[
-                                                Answer("Пазли", on_js_answer),
-                                                Answer("Простi, такi як Subway Surfers", on_cs_answer()),
+                                                Answer("Пазли", lambda: score("JavaScript")),
+                                                Answer("Простi, такi як Subway Surfers", lambda: score("Unity")),
+                                                Answer("Складні, такі як PUBG", lambda: score("Unreal")),
                                             ]
                                        )
                                 ),
-                                Answer("Ігри в браузері", on_js_answer)
+                                Answer("Ігри в браузері", lambda: score("JavaScript"))
                             ]
                    )
             ),
@@ -202,10 +213,10 @@ def create_test():
                             title="Які технології тебе цікавлять?",
                             image="path/to/image6.png",
                             answers=[
-                                Answer("Машинне навчання", on_python_answer),
-                                Answer("Розробка ігор", on_cpp_answer),
-                                Answer("Розробка веб-сайтів", on_js_answer),
-                                Answer("Розробка мобільних додатків", on_ruby_answer)
+                                Answer("Машинне навчання", lambda: score("Python")),
+                                Answer("Розробка ігор", lambda: score("C++")),
+                                Answer("Розробка веб-сайтів", lambda: score("JavaScript")),
+                                Answer("Розробка мобільних додатків", lambda: score("Java")),
                             ]
                     )
             ),
